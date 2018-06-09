64bd3ccff339973d1af87bf2644e4099100991f7
sudoQ <vorga88@gmail.com>
2018-06-09 11:49:52 +0300

    move config in one file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const loadersConfig = require("./loaders");
+const postcssPresetEnv = require('postcss-preset-env');
+
 
 module.exports = {
   entry: './src/js/main.js',
@@ -14,7 +15,72 @@ module.exports = {
     port: 9000
   },
   devtool: "source-map",
-  module: loadersConfig,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        //exclude: /index\.html$/, //without this HtmlWebPackPlugin will not work with template file correctly
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader', options: {
+              sourceMap: true,
+              ident: 'postcss',
+              plugins: () => [
+                postcssPresetEnv(/* options */)
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebPackPlugin({
       title: 'Title',
